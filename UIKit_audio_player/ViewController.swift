f49b69fd237239da860952eab23ff6ae235a7fd6
GArifullinII <ildaridrisovich@gmail.com>
2022-10-01 23:21:36 +0400

    feat: added player

--- UIKit_audio_player/ViewController.swift
+++ UIKit_audio_player/ViewController.swift
@@ -6,14 +6,57 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player = AVAudioPlayer()
+    var slider = UISlider()
 
+    @IBOutlet var sliderVolume: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // slider
+        self.slider.frame = CGRect(x: 110, y: 480, width: 200, height: 23)
+        self.slider.minimumValue = 0.0
+        self.slider.maximumValue = 100
+        self.view.addSubview(slider)
+        self.slider.addTarget(self,
+                              action: #selector(changeSlider),
+                              for: .valueChanged)
+        
+        // player
+        do {
+            if let audioPath = Bundle.main.path(forResource: "I'm a soldier", ofType: "mp3") {
+            try player = AVAudioPlayer(contentsOf: URL(fileURLWithPath: audioPath))
+                self.slider.maximumValue = Float(player.duration)
+            }
+        } catch {
+            print("Error")
+        }
+        self.player.play()
     }
-
-
+    
+    // action
+    @IBAction func playButton(_ sender: UIButton) {
+        self.player.play()
+    }
+    
+    @IBAction func pauseButton(_ sender: UIButton) {
+        self.player.pause()
+    }
+    
+    @IBAction func actionSliderVolume(_ sender: UISlider) {
+        self.player.volume = self.sliderVolume.value
+    }
+    
+    // func changeSlider
+    @objc func changeSlider(sender: UISlider) {
+        if sender == slider {
+            self.player.currentTime = TimeInterval(sender.value)
+        }
+    }
 }
 
